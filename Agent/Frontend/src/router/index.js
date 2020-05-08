99bebf13e4f448540196afd0f1a2e83878caab4d
ananikolasevic <23nikolasevic@gmail.com>
2020-05-08 12:42:47 +0200

    added basic elements of cart page

--- Agent/Frontend/src/router/index.js
+++ Agent/Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomePage from "../views/HomePage.vue";
+import Cart from "../views/Cart.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "HomePage",
     component: HomePage,
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
   },
   // {
   //   path: "/about",
