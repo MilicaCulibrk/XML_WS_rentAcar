7043b37adbb20ae86e2f4f06ba8ddf14542f22ec
Masa Matovic <masa.matovic97@gmail.com>
2020-05-09 11:19:24 +0200

    Login and Registration with validation

---
+++ Agent/Frontend/src/components/LoginComponent.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="LoginDialog" max-width="600px">
+      <template v-slot:activator="{ on }">
+        <v-btn class="mx-1" text color="primary" v-on="on">
+          <span>Login</span>
+          <v-icon right>lock_open</v-icon>
+        </v-btn>
+      </template>
+      <v-card class="cardBorderColor">
+        <v-card-title>
+          <span class="primary--text font-italic headline" primary-title>Login</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-form ref="form">
+              <v-text-field
+                label="Email*"
+                color="black"
+                v-model="email"
+                required
+                :rules="emailRules"
+              ></v-text-field>
+
+              <v-text-field
+                color="black"
+                label="Password*"
+                v-model="password"
+                type="password"
+                required
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-container>
+        </v-card-text>
+        <v-card-actions class="pr-10 pb-10">
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="close">Close</v-btn>
+          <v-btn color="primary" @click="login">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    LoginDialog: false,
+    password: "",
+    passwordRules: [v => !!v || "Password is required"],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ]
+  }),
+  methods: {
+    login() {
+      if (this.$refs.form.validate()) {
+        console.log(this.password + " " + this.email);
+        this.close();
+      } else {
+        console.log("nije validno");
+      }
+    },
+    close() {
+      this.LoginDialog = false;
+      this.$refs.form.reset();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.cardBorderColor {
+  border-left: 1.5px solid #ff8a65;
+  border-top: 1.5px solid #ff8a65;
+  border-right: 1.5px solid #ff8a65;
+  border-bottom: 1.5px solid #ff8a65;
+}
+</style>
--- Agent/Frontend/src/components/NavBar.vue
+++ Agent/Frontend/src/components/NavBar.vue
@@ -7,28 +7,32 @@
         <span class="font-italic font-weight-bold">Rent-A-CaR</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text color="primary">  
+      <v-btn text color="primary">
         <span @click="openCart()">Cart</span>
         <v-icon right>shopping_cart</v-icon>
       </v-btn>
-      <v-btn text color="primary">  
-        <span>Login</span>
-        <v-icon right>lock_open</v-icon>
-      </v-btn>
-      <v-btn text color="primary">
-        <span>Register</span>
-        <v-icon right>person_pin</v-icon>
-      </v-btn>
+      <div class="mx-2">
+        <LoginComponent />
+      </div>
+      <div class="mx-2">
+        <RegistrationComponent />
+      </div>
     </v-toolbar>
   </nav>
 </template>
 
 <script>
+import LoginComponent from "@/components/LoginComponent.vue";
+import RegistrationComponent from "@/components/RegistrationComponent.vue";
 export default {
+  components: {
+    LoginComponent,
+    RegistrationComponent
+  },
 
   methods: {
     openCart() {
-      this.$router.push("/cart")
+      this.$router.push("/cart");
     }
   }
 };
