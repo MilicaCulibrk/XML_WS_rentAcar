ac38892f2458be7874f16e7147429a7bc62267e2
Masa Matovic <masa.matovic97@gmail.com>
2020-05-18 22:48:33 +0200

    connecting microservices with eureka service

--- Microservices/user/src/main/java/com/services/user/controller/LoginController.java
+++ Microservices/user/src/main/java/com/services/user/controller/LoginController.java
@@ -4,16 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/login")
+@RestController()
+@RequestMapping(value = "/login")
 public class LoginController {
 
     // u metodi se salje objekat koji ima atribute username i password
     //necemo implementirati za sad, zbog mogucnosti autetifikacije preko getway-a
-    @PostMapping("")
-    public ResponseEntity login (/*@RequestBody Authentication authentication*/)  {
+  /*  @PostMapping(value = "")
+    public ResponseEntity login (@RequestBody Authentication authentication)  {
 
         return new ResponseEntity(HttpStatus.OK);
-    }
+    }*/
 }
