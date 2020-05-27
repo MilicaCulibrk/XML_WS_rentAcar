63d67c0cbeaaef4a78742f9fa3cc5185642151fe
MilicaCulibrk <milica6.1.97thebest@gmail.com>
2020-05-27 14:41:58 +0200

    added microservices frontend

---
+++ Microservices/Frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- root vuetify component -->
+  <v-app>
+    <NavBar />
+    <v-content class="mx-12">
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar";
+
+export default {
+  name: "App",
+
+  components: { NavBar },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ Microservices/Frontend/src/views/admin/Codebook.vue
@@ -0,0 +1,287 @@
+<template>
+  <div>
+    <v-container class="pt-12 mt-12">
+      <v-row>
+        <!-- meni  -->
+        <v-col cols="1"></v-col>
+        <v-col cols="4">
+          <div>
+            <v-list rounded>
+              <v-list-item-group color="primary">
+                <v-list-item
+                  v-for="menuItem in menuItems"
+                  :key="menuItem"
+                  @click="expandMenuItem(menuItem)"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title class="primary--text" v-text="menuItem"></v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-icon>
+                    <v-icon color="primary">play_arrow</v-icon>
+                  </v-list-item-icon>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </div>
+        </v-col>
+        <!-- expand komponenta -->
+        <v-col cols="6" class="shrink">
+          <!-- Brend -->
+          <v-expand-x-transition>
+            <v-card v-show="expandBrand" elevation="20">
+              <div class="cardBorderColor shrink">
+                <v-list>
+                  <v-list-item v-for="brandItem in brandItems" :key="brandItem">
+                    <v-list-item-content>
+                      <v-list-item-title class="primary--text" v-text="brandItem"></v-list-item-title>
+                    </v-list-item-content>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>sync</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Change</span>
+                    </v-tooltip>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Delete</span>
+                    </v-tooltip>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary">
+                      <v-icon>add</v-icon>
+                      <span>add new</span>
+                    </v-btn>
+                  </v-list-item>
+                </v-list>
+              </div>
+            </v-card>
+          </v-expand-x-transition>
+          <!-- Model -->
+          <v-expand-x-transition>
+            <v-card v-show="expandModel" elevation="20">
+              <div class="cardBorderColor shrink">
+                <v-list>
+                  <v-list-item v-for="modelItem in modelItems" :key="modelItem">
+                    <v-list-item-content>
+                      <v-list-item-title class="primary--text" v-text="modelItem"></v-list-item-title>
+                    </v-list-item-content>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>sync</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Change</span>
+                    </v-tooltip>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Delete</span>
+                    </v-tooltip>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary">
+                      <v-icon>add</v-icon>
+                      <span>add new</span>
+                    </v-btn>
+                  </v-list-item>
+                </v-list>
+              </div>
+            </v-card>
+          </v-expand-x-transition>
+          <!-- Class -->
+          <v-expand-x-transition>
+            <v-card v-show="expandClass" elevation="20">
+              <div class="cardBorderColor">
+                <v-list>
+                  <v-list-item v-for="classItem in classItems" :key="classItem">
+                    <v-list-item-content>
+                      <v-list-item-title class="primary--text" v-text="classItem"></v-list-item-title>
+                    </v-list-item-content>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>sync</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Change</span>
+                    </v-tooltip>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Delete</span>
+                    </v-tooltip>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary">
+                      <v-icon>add</v-icon>
+                      <span>add new</span>
+                    </v-btn>
+                  </v-list-item>
+                </v-list>
+              </div>
+            </v-card>
+          </v-expand-x-transition>
+          <!-- Transmission -->
+          <v-expand-x-transition>
+            <v-card v-show="expandTransmission" elevation="20">
+              <div class="cardBorderColor">
+                <v-list>
+                  <v-list-item
+                    v-for="transmissionItem in transmissionItems"
+                    :key="transmissionItem"
+                  >
+                    <v-list-item-content>
+                      <v-list-item-title class="primary--text" v-text="transmissionItem"></v-list-item-title>
+                    </v-list-item-content>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>sync</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Change</span>
+                    </v-tooltip>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Delete</span>
+                    </v-tooltip>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary">
+                      <v-icon>add</v-icon>
+                      <span>add new</span>
+                    </v-btn>
+                  </v-list-item>
+                </v-list>
+              </div>
+            </v-card>
+          </v-expand-x-transition>
+          <!-- Gas -->
+          <v-expand-x-transition>
+            <v-card v-show="expandGas" elevation="20">
+              <div class="cardBorderColor">
+                <v-list>
+                  <v-list-item v-for="gasItem in gasItems" :key="gasItem">
+                    <v-list-item-content>
+                      <v-list-item-title class="primary--text" v-text="gasItem"></v-list-item-title>
+                    </v-list-item-content>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>sync</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Change</span>
+                    </v-tooltip>
+                    <v-tooltip bottom color="black">
+                      <template v-slot:activator="{ on }">
+                        <v-btn icon v-on="on" color="primary">
+                          <v-icon>delete</v-icon>
+                        </v-btn>
+                      </template>
+                      <span class="primary--text">Delete</span>
+                    </v-tooltip>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-spacer></v-spacer>
+                    <v-btn color="primary">
+                      <v-icon>add</v-icon>
+                      <span>add new</span>
+                    </v-btn>
+                  </v-list-item>
+                </v-list>
+              </div>
+            </v-card>
+          </v-expand-x-transition>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expandBrand: false,
+      expandModel: false,
+      expandClass: false,
+      expandTransmission: false,
+      expandGas: false,
+      menuItems: ["Brand", "Model", "Class", "Transmission type", "Gas type"],
+      brandItems: ["BMW", "Audi", "Mercedes", "Tesla"],
+      modelItems: ["(Citroen) M5", "(Audi) R8", "(BMW) X6"],
+      classItems: ["SUV", "oldtimer", "city-car"],
+      transmissionItems: ["manual", "automatic", "semi-automatic"],
+      gasItems: ["gasoline", "gas", "diesel"]
+    };
+  },
+  methods: {
+    expandMenuItem(menuItem) {
+      if (menuItem == "Brand") {
+        this.cancelOtherMenus(menuItem);
+        this.expandBrand = !this.expandBrand;
+      } else if (menuItem == "Model") {
+        this.cancelOtherMenus(menuItem);
+        this.expandModel = !this.expandModel;
+      } else if (menuItem == "Class") {
+        this.cancelOtherMenus(menuItem);
+        this.expandClass = !this.expandClass;
+      } else if (menuItem == "Transmission type") {
+        this.cancelOtherMenus(menuItem);
+        this.expandTransmission = !this.expandTransmission;
+      } else if (menuItem == "Gas type") {
+        this.cancelOtherMenus(menuItem);
+        this.expandGas = !this.expandGas;
+      }
+    },
+    cancelOtherMenus(menuItem) {
+      if (menuItem != "Brand") {
+        this.expandBrand = false;
+      }
+      if (menuItem != "Model") {
+        this.expandModel = false;
+      }
+      if (menuItem != "Class") {
+        this.expandClass = false;
+      }
+      if (menuItem != "Transmission type") {
+        this.expandTransmission = false;
+      }
+      if (menuItem != "Gas type") {
+        this.expandGas = false;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.cardBorderColor {
+  border-left: 1px solid #fbc02d;
+  border-top: 1px solid #fbc02d;
+  border-right: 1px solid #fbc02d;
+  border-bottom: 1px solid #fbc02d;
+}
+</style>
